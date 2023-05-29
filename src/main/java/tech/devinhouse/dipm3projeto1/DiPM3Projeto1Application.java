9032b61edb74d28c7f2ce6fe5033011b105f3142
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-05-29 19:17:23 -0300

    Initial commit

---
+++ src/main/java/tech/devinhouse/dipm3projeto1/DiPM3Projeto1Application.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.dipm3projeto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiPM3Projeto1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiPM3Projeto1Application.class, args);
+    }
+
+}
---
+++ src/test/java/tech/devinhouse/dipm3projeto1/DiPM3Projeto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.dipm3projeto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiPM3Projeto1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
