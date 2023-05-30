b5372b7a436b1f4085c410ea44093eb6da2fad36
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-05-30 20:16:43 -0300

    Insere GETTERs nos ResponseDto.

--- src/main/java/tech/devinhouse/dipm3projeto1/dto/ConfirmacaoResponseDto.java
+++ src/main/java/tech/devinhouse/dipm3projeto1/dto/ConfirmacaoResponseDto.java
@@ -1,13 +1,17 @@
 package tech.devinhouse.dipm3projeto1.dto;
 
+import lombok.Getter;
 import tech.devinhouse.dipm3projeto1.model.Confirmacao;
 
+import java.time.format.DateTimeFormatter;
+
+@Getter
 public class ConfirmacaoResponseDto {
     private final String eticket;
     private final String dataHoraConfirmacao;
 
     public ConfirmacaoResponseDto(Confirmacao confirmacao) {
         eticket = confirmacao.getEticket();
-        dataHoraConfirmacao = confirmacao.getDataHoraConfirmacao().toString();
+        dataHoraConfirmacao = confirmacao.getDataHoraConfirmacao().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
     }
 }
--- src/main/java/tech/devinhouse/dipm3projeto1/dto/PassageiroResponseDto.java
+++ src/main/java/tech/devinhouse/dipm3projeto1/dto/PassageiroResponseDto.java
@@ -1,7 +1,11 @@
 package tech.devinhouse.dipm3projeto1.dto;
 
+import lombok.Getter;
 import tech.devinhouse.dipm3projeto1.model.Passageiro;
 
+import java.time.format.DateTimeFormatter;
+
+@Getter
 public class PassageiroResponseDto {
     private final String cpf;
     private final String nome;
@@ -12,7 +16,7 @@ public class PassageiroResponseDto {
     public PassageiroResponseDto(Passageiro passageiro) {
         cpf = passageiro.getCpf();
         nome = passageiro.getNome();
-        dataNascimento = passageiro.getDataNascimento().toString();
+        dataNascimento = passageiro.getDataNascimento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         classificacao = passageiro.getClassificacao().toString();
         milhas = passageiro.getMilhas();
     }
