77d8628c45d664762cd55b5e88bbc3fc31107089
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-05-30 18:03:40 -0300

    Cria endpoints de consulta de assentos.

--- src/main/java/tech/devinhouse/dipm3projeto1/controller/AssentosController.java
+++ src/main/java/tech/devinhouse/dipm3projeto1/controller/AssentosController.java
@@ -1,7 +1,23 @@
 package tech.devinhouse.dipm3projeto1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.devinhouse.dipm3projeto1.service.AssentosService;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/assentos")
 public class AssentosController {
+    private final AssentosService service;
+
+    public AssentosController(AssentosService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<String> findAll() {
+        return service.findAll();
+    }
 }
--- src/main/java/tech/devinhouse/dipm3projeto1/service/AssentosService.java
+++ src/main/java/tech/devinhouse/dipm3projeto1/service/AssentosService.java
@@ -2,6 +2,26 @@ package tech.devinhouse.dipm3projeto1.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AssentosService {
+    private final int NUMERO_DE_FILEIRAS = 10;
+    private final int ASSENTOS_POR_FILEIRA = 6;
+    private final List<String> assentosRepository;
+
+    public AssentosService() {
+        assentosRepository = new ArrayList<>();
+
+        for (int i = 1; i <= NUMERO_DE_FILEIRAS; i++)
+            for (int j = 0; j < ASSENTOS_POR_FILEIRA; j++) {
+                String assento = String.valueOf(i) + (char) ('A' + j);
+                assentosRepository.add(assento);
+            }
+    }
+
+    public List<String> findAll() {
+        return assentosRepository;
+    }
 }
