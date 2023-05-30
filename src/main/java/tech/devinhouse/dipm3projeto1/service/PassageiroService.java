a50c0d0eb774e25a2d688995a6ffac948e5b84de
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-05-30 17:38:42 -0300

    Cria endpoint de consulta por CPF.

--- src/main/java/tech/devinhouse/dipm3projeto1/service/PassageiroService.java
+++ src/main/java/tech/devinhouse/dipm3projeto1/service/PassageiroService.java
@@ -1,7 +1,21 @@
 package tech.devinhouse.dipm3projeto1.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import tech.devinhouse.dipm3projeto1.model.Passageiro;
+import tech.devinhouse.dipm3projeto1.repository.PassageiroRepository;
 
 @Service
 public class PassageiroService {
+    private final PassageiroRepository passageiroRepository;
+
+    public PassageiroService(PassageiroRepository passageiroRepository) {
+        this.passageiroRepository = passageiroRepository;
+    }
+
+    public Passageiro findByCpf(String cpf) {
+        return passageiroRepository.findById(cpf).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
